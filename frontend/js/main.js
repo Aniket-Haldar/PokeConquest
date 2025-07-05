0c5ecc5b9b0d87630f4379695a992dec3b7a46dd
Aniket-Haldar <aniket.haldar05@gmail.com>
2025-07-06 03:50:49 +0530

    Changed Endpoints

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -7,7 +7,7 @@ function startGame() {
     return;
   }
 
-  fetch("http://localhost:8080/api/users", {
+  fetch("https://pokeconquest.onrender.com//api/users", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ username })
