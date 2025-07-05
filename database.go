535aabc54cafc83e24b0d3f75dc1c6ac2e1e5cce
Aniket-Haldar <aniket.haldar05@gmail.com>
2025-07-06 03:29:29 +0530

    Database COnfig

--- database.go
+++ database.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,43 +11,50 @@ import (
 var DB *gorm.DB
 
 func initDatabase() {
-	// Load DB settings from environment variables
-	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
-	dbUser := os.Getenv("DB_USER")
-	dbPass := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
+	// Check for DATABASE_URL (Render production)
+	dbURL := os.Getenv("DATABASE_URL")
 
-	// Fallback defaults for local dev
-	if dbHost == "" {
-		dbHost = "localhost"
-	}
-	if dbPort == "" {
-		dbPort = "5432"
-	}
-	if dbUser == "" {
-		dbUser = "pokeuser"
-	}
-	if dbPass == "" {
-		dbPass = "pokepass"
-	}
-	if dbName == "" {
-		dbName = "pokequest"
+	// If DATABASE_URL is not set, build from individual vars (local dev)
+	if dbURL == "" {
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		dbUser := os.Getenv("DB_USER")
+		dbPass := os.Getenv("DB_PASSWORD")
+		dbName := os.Getenv("DB_NAME")
+
+		// Fallback defaults for local development
+		if dbHost == "" {
+			dbHost = "localhost"
+		}
+		if dbPort == "" {
+			dbPort = "5432"
+		}
+		if dbUser == "" {
+			dbUser = "pokeuser"
+		}
+		if dbPass == "" {
+			dbPass = "pokepass"
+		}
+		if dbName == "" {
+			dbName = "pokequest"
+		}
+
+		dbURL = "host=" + dbHost +
+			" user=" + dbUser +
+			" password=" + dbPass +
+			" dbname=" + dbName +
+			" port=" + dbPort +
+			" sslmode=disable"
 	}
 
-	// Connection string
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		dbHost, dbUser, dbPass, dbName, dbPort,
-	)
-
-	// Connect to DB
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// Connect to database
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 	if err != nil {
-		log.Fatal("❌ Failed to connect to database: ", err)
-		os.Exit(1)
+		log.Fatalf("❌ Failed to connect to database: %v", err)
 	}
+
+	// Assign global DB
+	DB = db
 
 	// Auto-migrate schema
 	err = DB.AutoMigrate(&User{}, &Pokemon{}, &Challenge{})
